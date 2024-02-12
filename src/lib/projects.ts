286e7b90534367bfc671c243b7433a2e10ed119e
Jose Rojas <jgra11.2010@gmail.com>
2024-02-12 18:16:20 -0400

    added other projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,20 +1,63 @@
-import { AstroIcon, ReactIcon, SupabaseIcon, TailwindIcon } from '@/components/Icons'
+import {
+	AstroIcon,
+	NextJSIcon,
+	ReactIcon,
+	SupabaseIcon,
+	TailwindIcon,
+	TypeScriptIcon
+} from '@/components/Icons'
 
+/* TODO: AGREGAR URL */
 export const works = [
 	{
 		id: 1,
-		domain: 'thecultureclan.com',
+		name: 'The Culture Clan',
 		description:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quo aliquam ratione ut repellat, necessitatibus sed sequi blanditiis quos! Blanditiis saepe voluptas earum corrupti quia possimus',
 		image: '/works/work-1.webp',
-		stack: [AstroIcon, ReactIcon, TailwindIcon]
+		stack: [AstroIcon, ReactIcon, TailwindIcon, TypeScriptIcon],
+		url: '#'
 	},
 	{
 		id: 2,
-		domain: 'koalavzla.com',
+		name: 'Koala Fitness Store',
 		description:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quo aliquam ratione ut repellat, necessitatibus sed sequi blanditiis quos! Blanditiis saepe voluptas earum corrupti quia possimus',
 		image: '/works/work-2.webp',
-		stack: [AstroIcon, ReactIcon, TailwindIcon, SupabaseIcon]
+		stack: [AstroIcon, ReactIcon, TailwindIcon, SupabaseIcon, TypeScriptIcon],
+		url: '#'
+	}
+]
+
+export const projects = [
+	{
+		id: 1,
+		name: 'Larri AI',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quo aliquam ratione ut repellat, necessitatibus sed sequi blanditiis quos! Blanditiis saepe voluptas earum corrupti quia possimus',
+		image: '/projects/larri.png',
+		stack: [NextJSIcon, TailwindIcon, TypeScriptIcon],
+		url: '#'
+	}
+]
+
+export const clones = [
+	{
+		id: 1,
+		name: 'Premios Esland',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quo aliquam ratione ut repellat, necessitatibus sed sequi blanditiis quos! Blanditiis saepe voluptas earum corrupti quia possimus',
+		image: '/clones/esland.webp',
+		stack: [AstroIcon, ReactIcon, TailwindIcon, TypeScriptIcon],
+		url: '#'
+	},
+	{
+		id: 2,
+		name: 'Spotify',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quo aliquam ratione ut repellat, necessitatibus sed sequi blanditiis quos! Blanditiis saepe voluptas earum corrupti quia possimus',
+		image: '/clones/spotify.png',
+		stack: [AstroIcon, ReactIcon, TailwindIcon, TypeScriptIcon],
+		url: '#'
 	}
 ]
