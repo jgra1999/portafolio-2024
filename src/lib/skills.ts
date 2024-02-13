98efdd086b36229ee33a69b4c203654c64b3471e
Jose Rojas <jgra11.2010@gmail.com>
2024-02-13 18:59:56 -0400

    added new icons and created the skills data

---
+++ src/lib/skills.ts
@@ -0,0 +1,45 @@
+import {
+	AstroIcon2,
+	GitIcon,
+	NextJSIcon,
+	NodeJSIcon,
+	ReactIcon,
+	SupabaseIcon,
+	TailwindIcon,
+	TypeScriptIcon
+} from '@/components/Icons'
+
+export const skills = [
+	{
+		name: 'ReactJS',
+		icon: ReactIcon
+	},
+	{
+		name: 'NextJS',
+		icon: NextJSIcon
+	},
+	{
+		name: 'Astro',
+		icon: AstroIcon2
+	},
+	{
+		name: 'Supabase',
+		icon: SupabaseIcon
+	},
+	{
+		name: 'TypeScript',
+		icon: TypeScriptIcon
+	},
+	{
+		name: 'TailwindCss',
+		icon: TailwindIcon
+	},
+	{
+		name: 'NodeJS',
+		icon: NodeJSIcon
+	},
+	{
+		name: 'Git',
+		icon: GitIcon
+	}
+]
