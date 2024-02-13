b88c6e54903dd9fb32a7479c2cf9613d1d4dfeb6
Jose Rojas <jgra11.2010@gmail.com>
2024-02-13 19:00:35 -0400

    skills carrousel created and added to skills section

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { SkillsCarousel } from '../skills/SkillsCarousel'
 
 export function Skills() {
 	return (
-		<div className='bg-[hsl(0,0%,6%)] flex flex-col items-center w-4/5 mx-auto container rounded-xl p-5 mb-10 text-center'>
-			<h2>Skills</h2>
-			<p className='max-w-[600px]'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro unde ad natus
-				in vitae alias excepturi autem quia eveniet, error esse consectetur amet
-				exercitationem explicabo possimus aut nesciunt animi sequi.
-			</p>
+		<div className='bg-[hsl(0,0%,6%)] flex flex-col gap-y-8 items-center w-4/5 mx-auto container rounded-xl p-5 mb-10 text-center'>
+			<div>
+				<h2>Skills</h2>
+				<p className='max-w-[600px]'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro unde ad
+					natus in vitae alias excepturi autem quia eveniet, error esse consectetur
+					amet exercitationem explicabo possimus aut nesciunt animi sequi.
+				</p>
+			</div>
+			<SkillsCarousel />
 		</div>
 	)
 }
---
+++ src/components/skills/SkillsCarousel.tsx
@@ -0,0 +1,28 @@
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious
+} from '@/components/ui/carousel'
+import { ReactIcon } from '../Icons'
+import { skills } from '@/lib/skills'
+
+export function SkillsCarousel() {
+	return (
+		<Carousel opts={{ loop: true }}>
+			<CarouselContent>
+				{skills.map((skill, index) => (
+					<CarouselItem key={index} className='md:basis-1/2 lg:basis-1/3'>
+						<div className='flex flex-col items-center gap-y-2'>
+							<skill.icon styles='w-36 h-36' />
+							<span className='text-sm font-semibold'>{skill.name}</span>
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+			<CarouselPrevious />
+			<CarouselNext />
+		</Carousel>
+	)
+}
