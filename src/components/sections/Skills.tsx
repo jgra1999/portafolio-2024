73e2a060970787741dabf846210561097fedc464
Jose Rojas <jgra11.2010@gmail.com>
2024-02-14 12:34:16 -0400

    Section HTML tag added to the component

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -3,16 +3,18 @@ import { SkillsCarousel } from '../skills/SkillsCarousel'
 
 export function Skills() {
 	return (
-		<div className='bg-[hsl(0,0%,6%)] flex flex-col gap-y-8 items-center w-4/5 mx-auto container rounded-xl p-5 mb-10 text-center'>
-			<div>
-				<h2>Skills</h2>
-				<p className='max-w-[600px]'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro unde ad
-					natus in vitae alias excepturi autem quia eveniet, error esse consectetur
-					amet exercitationem explicabo possimus aut nesciunt animi sequi.
-				</p>
+		<section className='container mb-10 text-center w-full relative'>
+			<div className='bg-[hsl(0,0%,6%)] flex flex-col gap-y-8 items-center w-4/5 mx-auto rounded-xl px-5 pt-5 pb-10'>
+				<div>
+					<h2>Skills</h2>
+					<p className='max-w-[600px]'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro unde ad
+						natus in vitae alias excepturi autem quia eveniet, error esse consectetur
+						amet exercitationem explicabo possimus aut nesciunt animi sequi.
+					</p>
+				</div>
+				<SkillsCarousel />
 			</div>
-			<SkillsCarousel />
-		</div>
+		</section>
 	)
 }
