07b6911215adcffe2797c69966e426fc7782240c
Jose Rojas <jgra11.2010@gmail.com>
2024-02-15 17:14:06 -0400

    files from the app folder relocated to the locale folder

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,20 @@
-import Image from 'next/image'
-import Navbar from '../components/Navbar'
+import Navbar from '../../components/Navbar'
 import Intro from '@/components/sections/Intro'
 import { Projects } from '@/components/sections/Projects'
 import { Skills } from '@/components/sections/Skills'
 import Contact from '@/components/sections/Contact'
 import Footer from '@/components/Footer'
 
+//? Translates
+import { useTranslations } from 'next-intl'
+
 export default function Home() {
+	const navT = useTranslations('Navbar')
+	const t = useTranslations('Index')
 	return (
 		<>
-			<Navbar />
-			<Intro />
+			<Navbar link1={navT('home')} link2={navT('projects')} link3={navT('skills')} />
+			<Intro greetings={t('greetings')} />
 			<Projects />
 			<Skills />
 			<Contact />
