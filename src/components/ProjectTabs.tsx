ec912c19df41da67558fc8c5da1bbc5eb907d905
Jose Rojas <jgra11.2010@gmail.com>
2024-03-07 17:44:21 -0400

    configured the min height of the project tabs

--- src/components/ProjectTabs.tsx
+++ src/components/ProjectTabs.tsx
@@ -21,7 +21,7 @@ export function ProjectTabs({ tabs }: Props) {
 						</Tab>
 					))}
 				</Tab.List>
-				<Tab.Panels className='mt-10'>
+				<Tab.Panels className='mt-10 min-h-[228px]'>
 					<Tab.Panel className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
 						{works.map((project) => (
 							<ProjectCard
